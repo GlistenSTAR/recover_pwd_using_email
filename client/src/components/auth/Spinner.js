7870e7ad2362fee6fdf9250230a6da086161a1ae
GlistenSTAR <glistenstar00@gmail.com>
2020-12-14 20:09:57 +0100

    clients components adding

--- client/src/components/auth/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSync } from '@fortawesome/free-solid-svg-icons';
-
-export default props =>
-  <div className={`fadeIn ${props.spinning}`}>
-    <FontAwesomeIcon icon={faSync} size={props.size} />
-  </div>  
--- client/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <footer className="text-white p-3 text-center">
-      Copyright &copy; {new Date().getFullYear()} Admission System
-    </footer>
-  );
-};
